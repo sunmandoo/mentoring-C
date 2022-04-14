7e9388fec050a4400fcde73a86664cb0e6420b6a
sunmandoo <dhkdaksenzlffj@gmail.com>
2022-04-14 21:11:46 +0900

    ㅁㅌㅁㅌ

---
+++ ㅁㅌㅁㅌ/1-9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int line = 1; line <= 5; line ++)
+    {
+        for (int star = 0; star < line; star++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    int star1;
+    scanf_s("%d", &star1);
+    for (int line = 1; line <= star1; line++)
+    {
+        for (int star = 0; star < line; star++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ ㅁㅌㅁㅌ/2-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int line = 1; line <= 5; line++)
+    {
+        for (int space = 5; space > line; space--)
+        {
+            printf(" ");
+        }
+        for (int star = 0; star < line; star++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
