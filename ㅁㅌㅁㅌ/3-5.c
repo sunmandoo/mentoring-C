b5e0ba25936aefe9266b7c48972cebbfb22af516
sunmandoo <dhkdaksenzlffj@gmail.com>
2022-05-02 13:31:15 +0900

    ㅁㅌㅁㅌ

---
+++ ㅁㅌㅁㅌ/3-5.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+	int k = 0;
+	int j = 0;
+	int l = 0;
+	int h = 3;
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int up_space = 0; up_space < j; up_space++)
+		{
+			printf(" ");
+		}
+		j = j + 1;
+
+		for (int up_star = 9; up_star > k; up_star--)
+		{
+			printf("*");
+		}
+		k = k + 2;
+
+		printf("\n");
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int down_space = 3; down_space > l; down_space--)
+		{
+			printf(" ");
+		}
+		l++;
+
+		for (int down_star = 0; down_star < h; down_star++)
+		{
+			printf("*");
+		}
+		h = h + 2;
+
+		printf("\n");
+	}
+
+	return 0;
+}
+
+/*
+            *********
+             *******
+              *****
+               ***
+                *
+               ***
+              *****
+             *******
+            *********
+
+*/
---
+++ ㅁㅌㅁㅌ/3-6.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 1; i < 11; i++)
+	{
+		for (int leftup_star = 0; leftup_star < i; leftup_star++)
+		{
+			printf("*");
+		}
+
+		for (int leftup_space = 11; leftup_space > i; leftup_space--)
+		{
+			printf(" ");
+		}
+
+		for (int rightup_star = 11; rightup_star > i; rightup_star--)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+
+	for (int i = 1; i < 11; i++)
+	{
+		for (int leftdown_space = 11; leftdown_space > i; leftdown_space--)
+		{
+			printf(" ");
+		}
+
+		for (int leftdown_star = 0; leftdown_star < i; leftdown_star++)
+		{
+			printf("*");
+		}
+		
+		for (int rightdown_space = 0; rightdown_space < i; rightdown_space++)
+		{
+			printf(" ");
+		}
+
+		for (int rightdown_star = 11; rightdown_star > i; rightdown_star--)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
